514359571115f2405c765243cc696bad1682330a
Tibor Bodecs <mail.tib@gmail.com>
2024-05-17 12:26:54 +0200

    current year support

--- Sources/Toucan/TemplateLibrary/Contexts/ContentContext.swift
+++ Sources/Toucan/TemplateLibrary/Contexts/ContentContext.swift
@@ -5,9 +5,28 @@
 //  Created by Tibor Bodecs on 10/05/2024.
 //
 
+import Foundation
+
 struct ContentContext<T> {
     let site: SiteContext
     let metadata: MetadataContext
     let content: T
+    let currentYear: String
     let userDefined: [String: String]
+    
+    init(
+        site: SiteContext,
+        metadata: MetadataContext,
+        content: T,
+        userDefined: [String : String]
+    ) {
+        let year = Calendar(identifier: .gregorian)
+            .component(.year, from: Date())
+        self.site = site
+        self.metadata = metadata
+        self.content = content
+        self.currentYear = "\(year)"
+        self.userDefined = userDefined
+    }
 }
+
