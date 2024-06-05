2b13f74e7d964bc8ed975476485b8235d09548a7
Tibor Bodecs <mail.tib@gmail.com>
2024-06-05 10:01:03 +0200

    remove dist

--- Tests/sites/binarybirds.com/dist/js/copyright.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function () {
-  var currentYear = new Date().getFullYear();
-  var copyrightSpan = document.getElementById('copyright');
-  copyrightSpan.textContent =
-    'Binary Birds © 2022-' + currentYear + ' - All Rights Reserved';
-});
