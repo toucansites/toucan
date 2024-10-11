b88c6006ca8bc416cddf3c631125dc5f75fc9806
David Kennedy <github@zenopolis.com>
2024-10-11 21:10:14 +0100

    Update install-toucan.sh
    For machines that are affected by bug #24, these changes create the /usr/local/bin directory.
    
    For this to work on those machines the make command needs to be called with sudo...
    
    `sudo make install`
    
    (This will require the admin password).

--- scripts/install-toucan.sh
+++ scripts/install-toucan.sh
@@ -5,6 +5,11 @@ log() { printf -- "** %s\n" "$*" >&2; }
 error() { printf -- "** ERROR: %s\n" "$*" >&2; }
 fatal() { error "$@"; exit 1; }
 
+if [ ! -d "/usr/local/bin" ]; then
+    echo "Creating directory /usr/local/bin"
+    mkdir -p "/usr/local/bin"
+    echo "/usr/local/bin directory created"
+fi
 
 swift build -c release
 install .build/release/toucan-cli /usr/local/bin/toucan
