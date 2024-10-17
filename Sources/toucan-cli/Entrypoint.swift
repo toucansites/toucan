1e3dfc3bdf44587583e15d4fb10b5b548f64725d
Tibor Bödecs <mail.tib@gmail.com>
2024-10-17 17:22:45 +0200

    Alpha.3 release (#38)
    - update version number

--- Sources/toucan-cli/Entrypoint.swift
+++ Sources/toucan-cli/Entrypoint.swift
@@ -14,7 +14,7 @@ struct Entrypoint: AsyncParsableCommand {
         discussion: """
             A markdown-based Static Site Generator (SSG) written in Swift.
             """,
-        version: "1.0.0-alpha.2",
+        version: "1.0.0-alpha.3",
         subcommands: [
             Generate.self,
             Serve.self,
