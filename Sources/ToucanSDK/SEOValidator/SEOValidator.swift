088d71cc5972d121e737dd2e6017a7c348c8811e
Tibor Bodecs <mail.tib@gmail.com>
2024-10-31 00:25:58 +0800

    heading validation basics

--- Sources/ToucanSDK/SEOValidator/SEOValidator.swift
+++ Sources/ToucanSDK/SEOValidator/SEOValidator.swift
@@ -9,26 +9,114 @@ import Foundation
 import Logging
 import SwiftSoup
 
-struct SEOValidator {
+extension SwiftSoup.Document {
+
+    public func selectFirst(_ query: String) throws -> Element? {
+        try select(query).first()
+    }
+
+    func getTitle() throws -> String? {
+        try selectFirst("title")?.text()
+    }
+
+    //    func getAttribute(_ key: String) throws -> String? {
+    //        try attr(key)
+    //    }
+}
+
+public struct SEOValidator {
+
+    public enum Error: Swift.Error {
+        case validation(String)
+    }
 
     let logger: Logger
+
+    public init(logger: Logger) {
+        self.logger = logger
+    }
 
     func validate(
         html: String,
         using pageBundle: PageBundle
     ) {
+        let metadata: Logger.Metadata = [
+            "type": "\(pageBundle.contentType.id)",
+            "slug": "\(pageBundle.slug)",
+        ]
+
         do {
             let document: SwiftSoup.Document = try SwiftSoup.parse(html)
-            let htmlTag = try document.select("html")
-            //            print(htmlTag.first()?.nodeName())
-            //            let headings = try document.select("h1, h2, h3, h4, h5, h6")
-            //            print("---------------------------")
-            //            for element in headings {
-            //                print(element.nodeName())
-            //                let text = try element.text()
-            //                let level = element.nodeName().hasSuffix("2") ? 2 : 3
-            //                let fragment = try element.attr("id")
+            guard let title = try document.getTitle() else {
+                throw Error.validation("Title not found")
+            }
+            //            if title.count < 55 {
+            //                logger.warning(
+            //                    "Title is too short, use minimum 55 characters.",
+            //                    metadata: metadata
+            //                )
             //            }
+            //            if title.count > 65 {
+            //                logger.warning(
+            //                    "Title is too long, use maximum 65 characters.",
+            //                    metadata: metadata
+            //                )
+            //            }
+            //            if title.count > 70 {
+            //                logger.error(
+            //                    "Title is way too long, use maximum 70 characters.",
+            //                    metadata: metadata
+            //                )
+            //            }
+
+            var isCanonicalLinkPresent = false
+            let links = try document.select("link")
+            for link in links {
+                let rel = try link.attr("rel")
+                if rel == "canonical" {
+                    isCanonicalLinkPresent = true
+                }
+            }
+            if !isCanonicalLinkPresent {
+                logger.warning(
+                    "Canonical link not present",
+                    metadata: metadata
+                )
+            }
+
+            if let keyword = pageBundle.frontMatter.string("keyword") {
+                if !title.contains(keyword) {
+                    logger.warning(
+                        "Title does not contain keyword: `\(keyword)`.",
+                        metadata: metadata
+                    )
+                }
+            }
+
+            let headings = try document.select("h1, h2, h3, h4, h5, h6")
+            var currentLevel = 1
+
+            for element in headings {
+                guard let level = Int(element.nodeName().dropFirst()) else {
+                    logger.error("Invalid heading level.")
+                    continue
+                }
+                let text = try element.text()
+                if level == 1 {
+                    if text.count > 80 {
+                        logger.warning(
+                            "Heading 1 should be 80 characters or less."
+                        )
+                    }
+                }
+
+                if level > currentLevel + 1 {
+                    logger.warning("Missing heading level \(currentLevel + 1).")
+                }
+                currentLevel = level
+
+                //                print(text)
+            }
         }
         catch Exception.Error(_, let message) {
             logger.error("\(message)")
@@ -89,8 +177,8 @@ struct SEOValidator {
         }
 
         // check keyword
-        let keyword = pageBundle.frontMatter.string("keyword")
-        if let keyword {
+
+        if let keyword = pageBundle.frontMatter.string("keyword") {
             if !pageBundle.title.contains(keyword) {
                 logger.warning(
                     "Title does not contain keyword: `\(keyword)`.",
@@ -104,6 +192,5 @@ struct SEOValidator {
                 )
             }
         }
-
     }
 }
