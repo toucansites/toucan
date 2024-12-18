cfde03889f6a17e12a2e5a47163e6f55fe657e86
Tibor Bödecs <mail.tib@gmail.com>
2024-12-18 15:42:24 +0100

    Fix custom user defined keys for RSS' (#58)
    - add support for global user defined keys 
    - add support for page bundle user defined keys

--- Sources/ToucanSDK/Context/RSSContext.swift
+++ Sources/ToucanSDK/Context/RSSContext.swift
@@ -12,6 +12,20 @@ struct RSSContext {
         let title: String
         let description: String
         let publicationDate: String
+
+        let userDefined: [String: Any]
+
+        var context: [String: Any] {
+            userDefined.recursivelyMerged(
+                with: [
+                    "permalink": permalink,
+                    "title": title,
+                    "description": description,
+                    "publicationDate": publicationDate,
+                ]
+            )
+            .sanitized()
+        }
     }
 
     let title: String
@@ -21,4 +35,20 @@ struct RSSContext {
     let lastBuildDate: String
     let publicationDate: String
     let items: [Item]
+    let userDefined: [String: Any]
+
+    var context: [String: Any] {
+        userDefined.recursivelyMerged(
+            with: [
+                "title": title,
+                "description": description,
+                "baseUrl": baseUrl,
+                "language": language as Any,
+                "lastBuildDate": lastBuildDate,
+                "publicationDate": publicationDate,
+                "items": items.map(\.context),
+            ]
+        )
+        .sanitized()
+    }
 }
