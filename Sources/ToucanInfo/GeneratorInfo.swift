582c359530f346417d946831b780b23643661b97
Tibor Bodecs <mail.tib@gmail.com>
2025-05-14 09:28:30 +0200

    update version & package versions

--- Sources/ToucanInfo/GeneratorInfo.swift
+++ Sources/ToucanInfo/GeneratorInfo.swift
@@ -38,7 +38,7 @@ public extension GeneratorInfo {
 
     /// Returns the most current version of the generator.
     static var current: Self {
-        .v1_0_0_beta_4
+        .v1_0_0_beta_5
     }
 }
 
@@ -46,6 +46,7 @@ public extension GeneratorInfo {
 extension GeneratorInfo {
 
     static let v1_0_0 = GeneratorInfo(version: "1.0.0")
+    static let v1_0_0_beta_5 = GeneratorInfo(version: "1.0.0-beta.5")
     static let v1_0_0_beta_4 = GeneratorInfo(version: "1.0.0-beta.4")
     static let v1_0_0_beta_3 = GeneratorInfo(version: "1.0.0-beta.3")
     static let v1_0_0_beta_2 = GeneratorInfo(version: "1.0.0-beta.2")
