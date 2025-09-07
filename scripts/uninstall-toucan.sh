96df17782dcf3e580b6e0e7a3607a64a6ca63f1d
Giovanni Tirloni <gtirloni@undead.sh>
2025-09-07 16:49:24 -0300

    scripts: Add ability to customize installation directory

--- scripts/uninstall-toucan.sh
+++ scripts/uninstall-toucan.sh
@@ -5,8 +5,8 @@ log() { printf -- "** %s\n" "$*" >&2; }
 error() { printf -- "** ERROR: %s\n" "$*" >&2; }
 fatal() { error "$@"; exit 1; }
 
-sudo rm -f /usr/local/bin/toucan
-sudo rm -f /usr/local/bin/toucan-generate
-sudo rm -f /usr/local/bin/toucan-init
-sudo rm -f /usr/local/bin/toucan-serve
-sudo rm -f /usr/local/bin/toucan-watch
+INSTALL_DIR="${1:-/usr/local/bin}"
+
+for binary in toucan toucan-generate toucan-init toucan-serve toucan-watch; do
+  sudo rm -f "${INSTALL_DIR}/${binary}"
+done
